de8b4cae79f9676466c2084ba0e8e6261497c89b
Robert Leggat <rleg7804@SHARED.SYDNEY.EDU.AU>
2019-01-10 11:22:00 +1100

    Added app variable because i removed it but it is back now

--- demobot.py
+++ demobot.py
@@ -1,12 +1,10 @@
-
 from flask import Flask, request
 
-...
+app = Flask(__name__)
 
 @app.route('/greet')
 def greet_person():
     name = request.values.get('text')
     return f'hi {name}!'
-
 
 app.run()

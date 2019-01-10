678ff47c3d98f1c373a4d0ff9d8e1735fd76db7e
Robert Leggat <rleg7804@SHARED.SYDNEY.EDU.AU>
2019-01-10 16:35:43 +1100

    put get and post in dice

--- weather.py
+++ weather.py
@@ -12,7 +12,7 @@ def weather():
     else:
         return f"She'll be right, it's only {temp}"
 
-@app.route('/dice' methods=['POST'])
+@app.route('/dice', methods=['GET', 'POST'])
 def dice():
     numbers = [1,2,3,4,5,6] 
     human = random.choice(numbers)

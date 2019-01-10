e98095054e7661ef13a3acfd278c560151454d1d
Robert Leggat <rleg7804@SHARED.SYDNEY.EDU.AU>
2019-01-10 11:14:08 +1100

    change for the user to greet the bot using /greet

--- demobot.py
+++ demobot.py
@@ -1,12 +1,10 @@
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Helloo, World!'
-
-if __name__ == '__main__':
-    
-    app.run()
+@app.route('/greet')
+def greet_person():
+    name = request.values.get('text')
+    return f'hi {name}!'
+app.run()

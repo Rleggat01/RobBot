61a8c2b5d63f3fb1f4e248a8695abcf8481809df
Robert Leggat <rleg7804@shared.sydney.edu.au>
2019-01-10 16:01:45 +1100

    Added the dice program to the weather.py

--- weather.py
+++ weather.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import random
 
 app = Flask(__name__)
 
@@ -10,6 +11,22 @@ def weather():
         return "Yeah nah nah yeah it's a tad hot outside I reckon"
     else:
         return f"She'll be right, it's only {temp}"
+
+@app.route('/dice')
+def dice():
+    numbers = [1,2,3,4,5,6] 
+    human = random.choice(numbers)
+    computer = random.choice(numbers)
+
+    ## return f'Human: {human} \n Computer: {computer}'
+    if human > computer:
+        
+        return f'Humans = {human} \n Computers = {computer} ... Humans win!'
+    elif human < computer:
+        return f'Humans = {human} \n Computers = {computer} ... Computers win!'
+    elif human == computer:
+        return f'Humans = {human} \n Computers = {computer} ... It is a tie!'
+
     
     
 if __name__ == '__main__':
